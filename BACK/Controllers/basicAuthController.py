95e18d01167d7a8f60fe581e2248eeaffedc52aa
STOLOWAR <andrey.stolov.04@gmail.com>
2024-09-27 13:40:35 +0300

    spring

--- BACK/Controllers/basicAuthController.py
+++
@@ -1,6 +0,0 @@
-from flask import request, Response
-
-def basic_auth():
-    if request.method.lower() == 'options':
-        return Response()
-    
--- BACK/Controllers/textController.py
+++
@@ -1,5 +0,0 @@
-from flask import request, Response
-
-def text_process(classifier):
-    text = request.json.get('inputValue', None)
-    return Response(classifier.summary(text), content_type="application/json")
