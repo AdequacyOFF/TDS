38e866c31ddd0cf6947de3184d1d3ffdee6c0c42
AdequacyOFF <adequacyoff-AO@yandex.ru>
2024-09-28 09:12:59 +0300

    Добавил вывод subtask при нажатии на кнопку

--- src/Pages/ProjectDetail.jsx
+++ src/Pages/ProjectDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import projects from './projects.json'; 
 import './../Pages_css/ProjectDetail.css'; 
@@ -6,6 +6,15 @@ import './../Pages_css/ProjectDetail.css';
 const ProjectDetail = () => {
   const { id } = useParams(); 
   const project = projects.find(p => p.project_id === id);
+
+  const [visibleSubTasks, setVisibleSubTasks] = useState({});
+
+  const toggleSubTasksVisibility = (index) => {
+    setVisibleSubTasks(prevState => ({
+      ...prevState,
+      [index]: !prevState[index]
+    }));
+  };
 
   if (!project) {
     return <div>Проект не найден</div>; 
@@ -28,17 +37,29 @@ const ProjectDetail = () => {
         <ul>
           {project.tasks.map((task, index) => (
             <li key={index}>
-              <strong>{task.name}:</strong> 
-              {task.description} 
-              (Прогресс: {task.progress}%)
-              <button className='SubTasks' onClick={
-                () => setSubTasksVisible(index,!subTasksVisible)
-              }>
-                Показать подзадачи
+              <strong>{task.name}:</strong> {task.description} (Прогресс: {task.progress}%)
+              <button 
+                className='SubTasks' 
+                onClick={() => toggleSubTasksVisibility(index)}
+              >
+                {visibleSubTasks[index] ? 'Скрыть подзадачи' : 'Показать подзадачи'}
               </button>
+
+              {visibleSubTasks[index] && (
+                <ul className="subtasks">
+                  {task.subtasks.map((subtask, subIndex) => (
+                    <li key={subIndex}>
+                      <strong>{subtask.name}:</strong> {subtask.description}
+                      <p><strong>Статус:</strong> {subtask.status}%</p>
+                      <p><strong>Исполнитель:</strong> {subtask.executor}</p>
+                      <p><strong>Начало:</strong> {subtask.begin}</p>
+                      <p><strong>Окончание:</strong> {subtask.end}</p>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           ))}
-          
         </ul>
       )}
       
