a51d24be09838582d566a1447c33e7a811276846
AdequacyOFF <adequacyoff-AO@yandex.ru>
2024-09-28 03:52:54 +0300

    Добавил список проектов и управление проектами

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import ProjectList from './ProjectList';  // Импортируем компонент списка проектов
 import '../Pages_css/Profile.css';
 
 function Profile() {
   return (
     <div className="Profile-wrapper">
-      <div className='Profile_photo' >
-          <img src="src/Images/Photo.png" alt=""  />
+      <div className='Profile_photo'>
+        <img src="src/Images/Photo.png" alt="Profile" />
       </div>
       <div className="User_info">
         <p className="Name">Name Sername</p>
@@ -17,38 +18,12 @@ function Profile() {
       </div>
       <div className="Search">
         <form action="">
-            <input type="text" placeholder="Присоединиться к проекту через название или #"/>
+          <input type="text" placeholder="Присоединиться к проекту через название или #" />
         </form>
       </div>
       <div className="Projects">
-        <h1> Мои проекты</h1>
-        <div className="Project_1">
-          <p className="Project_name">TESA 
-            <br />
-            #5432
-          </p>  
-          <p className="GM_info">
-            <p className="GM">
-                Руководитель:  
-                <Link to='/Profile'>
-                    Владислав Баев
-                </Link>
-            </p>
-            <br />
-            <p className="Now">
-                Текущее:
-            </p>
-          </p>
-          
-            <p className="Manage_project">
-                <Link to='/Manage'>
-                    Управлять проектом
-                </Link>
-            </p>
-            <p className="Progress">
-                70%
-            </p>
-        </div>
+        <h1>Мои проекты</h1>
+        <ProjectList />  {/* Вставляем компонент ProjectList, который отобразит все проекты */}
       </div>
     </div>
   );
